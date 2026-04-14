1c7aef784b03b7ad113dcc3da000c8b5ddcc1460
xMansour <MElsayedMansour96@gmail.com>
2026-04-14 13:19:18 +0200

    fix(payments): change get payment by order id to return a list instead of optional incase of multiple retries

--- src/main/java/us/exequt/ecommerce/payment/PaymentRepository.java
+++ src/main/java/us/exequt/ecommerce/payment/PaymentRepository.java
@@ -3,9 +3,9 @@ package us.exequt.ecommerce.payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import us.exequt.ecommerce.payment.domain.PaymentAttempt;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<PaymentAttempt, UUID> {
-    Optional<PaymentAttempt> findByOrderId(UUID orderId);
+    List<PaymentAttempt> findByOrderId(UUID orderId);
 }
