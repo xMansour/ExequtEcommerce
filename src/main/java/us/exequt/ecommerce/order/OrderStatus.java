bc23fffb5d6032c9d1e00f6e5eca526c6da52d69
xMansour <MElsayedMansour96@gmail.com>
2026-04-14 13:19:13 +0200

    feat(orders): order transition logic

--- src/main/java/us/exequt/ecommerce/order/OrderStatus.java
+++ src/main/java/us/exequt/ecommerce/order/OrderStatus.java
@@ -4,5 +4,14 @@ public enum OrderStatus {
     CREATED,
     PENDING_PAYMENT,
     PAYMENT_FAILED,
-    PAID
+    PAID;
+
+    public boolean canTransitionTo(OrderStatus newStatus) {
+        return switch (this) {
+            case CREATED -> newStatus == PENDING_PAYMENT || newStatus == PAYMENT_FAILED;
+            case PENDING_PAYMENT -> newStatus == PAID || newStatus == PAYMENT_FAILED;
+            case PAYMENT_FAILED -> newStatus == PENDING_PAYMENT;
+            case PAID -> false;
+        };
+    }
 }
